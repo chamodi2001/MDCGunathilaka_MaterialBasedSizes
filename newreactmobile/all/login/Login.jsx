9cba467ad2cb5d542566ef8d086d28fbfcc69570
chamodi2001 <chamodigunathilaka2018@gmail.com>
2024-02-26 23:55:57 +0530

    connecting to mysql post new

---
+++ newreactmobile/all/login/Login.jsx
@@ -0,0 +1,75 @@
+import React,{useState} from 'react';
+import { Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
+import { styleslogin } from './Loginstyles';
+import { styles } from '../register/Registerstyles';
+import { Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Register from '../register/Register';
+import EnterSize from '../usersize/Entersize';
+import axios from 'axios';
+
+
+const Login=()=> {
+    const navigation = useNavigation(); 
+
+    const [usernamelog, setunlog] = useState(''); 
+    const [passwordlog, setpwlog] = useState('');
+
+    const handleSubmit = async (event) => {
+        ////
+        event.preventDefault();
+        const Login = { usernamelog, passwordlog};
+        
+        try {
+        const output = await axios.get('http://localhost:8080/userlogin', Login);
+        window.alert('Successfully added data to the database');
+        console.log(output.data);
+        } catch (error) {
+        console.error('Error in adding a new user:', error);
+        }
+        ////
+        if (!usernamelog) {
+            //checks username is empty
+            Alert.alert('Error', 'Username is required');
+            return;//stops the function
+          }
+          if (!passwordlog) {
+              Alert.alert('Error', 'Password is required');
+              return;
+              }
+              else if(passwordlog.length < 4){
+                  Alert.alert('Error', 'Enter more than 4 characters');
+                  return;
+              }
+          else{
+              Alert.alert('Successful', 'successfully registered');
+              navigation.navigate('Bottomstack'); //obj.func
+              return;
+          }
+        //checks with data base, if the user exists, and 
+        //if the password matches the username
+        // if (!usernamelog) {
+        //   //checks username is empty
+        //   Alert.alert('Error', 'Username is required');
+        //   return;
+        // }
+    };
+    return (
+        <View style={styles.container}>
+            <Text style={styleslogin.title}>Login</Text>
+            <TextInput style={styles.input} placeholder="Username" onChangeText={text => setunlog(text)}
+            defaultValue={usernamelog} />
+            <TextInput style={styles.input} placeholder="Password" onChangeText={text => setpwlog(text)}
+                defaultValue={passwordlog} secureTextEntry/>
+            <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+                <Text style={styles.buttonText}>Login</Text>
+            </TouchableOpacity>
+            <Text style={styleslogin.text}>Don't have an account?</Text> 
+            <TouchableOpacity style={styleslogin.text} onPress={() => navigation.navigate('Register')}>
+                <Text style={styleslogin.textsign}>Sign Up</Text>
+            </TouchableOpacity>
+        </View>
+    );
+    };
+
+    export default Login;
--- springbootbackend/src/main/java/com/sizerecom/springbootbackend/repository/RegisterRepo.java
+++ springbootbackend/src/main/java/com/sizerecom/springbootbackend/repository/RegisterRepo.java
@@ -1,6 +1,7 @@
 package com.sizerecom.springbootbackend.repository;
 
 import com.sizerecom.springbootbackend.model.Register;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 //mapping between Java objects and database tables
 
